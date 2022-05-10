908378293840cb38b3941f371071b69ac10781c2
fabianoNsc <fabianont_123@yahoo.com.br>
2022-05-10 20:49:31 -0300

    Pagination Layout

--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -4,7 +4,7 @@ function Form() {
     const movie = {
         id: 1,
         image: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
-        title: "The Witcher, seriado(2 temporadas)",
+        title: "The Witcher, seriado (2 temporadas)",
         count: 2,
         score: 4.5
     };
