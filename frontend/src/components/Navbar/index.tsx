1d68b3ae4c67dae809e39e3b1ad711bf9d413603
fabianoNsc <fabianont_123@yahoo.com.br>
2022-05-10 20:33:56 -0300

    Routes

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -6,7 +6,7 @@ function Navbar() {
         <header>
             <nav className="container">
                 <div className="dsmovie-nav-content">
-                    <h1 className="dsmovie-contact-link">DSMovies</h1>
+                    <h1 >DSMovies</h1>
                     <a href="https://github.com/fabianoNsc">
                         <div className="dsmovie-contact-container">
                             <GithubIcon />
