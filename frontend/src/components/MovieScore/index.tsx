4e8b1ad63d0f3415e39d1cf8445ca4ff796fef86
fabianoNsc <fabianont_123@yahoo.com.br>
2022-05-10 22:59:43 -0300

    MovieCard

--- frontend/src/components/MovieScore/index.tsx
+++ frontend/src/components/MovieScore/index.tsx
@@ -7,7 +7,7 @@ function MovieScore() {
     return (
         <div className="dsmovie-score-container">
             <p className="dsmovie-score-value">{score > 0 ? score.toFixed(1) : '-'}</p>
-            <MovieScore />
+            <MovieStars />
             <p className="dsmovie-score-count">{count} avaliações</p>
         </div>
 
